a4720ec4c33c8e733356ab892eab52196aaf166f
Kürşat Coşkun <kursat.coskun@etiya.com>
2021-04-22 16:43:38 +0300

    added structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Dropdown } from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import logo from "./netflix.png";
 
 function App() {
+  const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
+  useEffect(() => {
+    getAllGenres();
+  }, []);
+
+  const getAllGenres = () => {
+    fetch(
+      "https://raw.githubusercontent.com/f/netflix-genres/main/genres.tr.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setOptions(data));
+  };
+
+  const onChange = (e, data) => {
+    setSelectedOption(data.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="item-container">
+        <img className="logo-img" alt="logo-netflix" src={logo} />
+      </div>
+
+      <div className="item-container">
+        <h2 className="header-text">Genre Search</h2>
+      </div>
+      <br />
+      <div className="item-container">
+        <div className="dropdown-wrapper">
+          <Dropdown
+            placeholder="Select or search Genre..."
+            search
+            fluid
+            selection
+            options={options.map((option) => ({
+              key: option.url,
+              value: option.url,
+              text: option.name,
+            }))}
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <br />
+      <div className="item-container">
+        <Button onClick={() => window.open(String(selectedOption))}>
+          Search on Netflix
+        </Button>
+      </div>
     </div>
   );
 }
